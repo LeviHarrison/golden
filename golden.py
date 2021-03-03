4d823ab87fc3f3b3ef45fee67ed439355d1e32ee
Levi Harrison <54278938+LeviHarrison@users.noreply.github.com>
2021-03-03 19:44:48 +0000

    detection

--- golden.py
+++ golden.py
@@ -10,6 +10,14 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
 corners = np.int0(corners)
 
+found = []
+def check_found(new, found):
+  for i in found:
+    if set(new) == set(i):
+      return True
+  
+  return False
+
 for i in range(len(corners)):
   for j in range(len(corners)):
     if j == i:
@@ -17,13 +25,10 @@ for i in range(len(corners)):
     for k in range(len(corners)):
       if k == j or k == i:
         continue
-
-      if round(math.dist(corners[i][0], corners[j][0]) / math.dist(corners[j][0], corners[k][0]), 1) == 1.6:
-        x,y = corners[i].ravel()
-        cv2.circle(img,(x,y),3,255,-1)
-        x,y = corners[j].ravel()
-        cv2.circle(img,(x,y),3,255,-1)
-        x,y = corners[k].ravel()
-        cv2.circle(img,(x,y),3,255,-1)
+      if round(math.dist(corners[i][0], corners[j][0]) / math.dist(corners[j][0], corners[k][0]), 3) == 1.618:
+        if not check_found([i, j, k], found):
+          img = cv2.line(img, tuple(corners[i][0]), tuple(corners[j][0]), (0, 0, 225), 1)
+          img = cv2.line(img, tuple(corners[j][0]), tuple(corners[k][0]), (255, 0, 0), 1)
+          found.append([i, j, k])
 
 cv2.imwrite("test.png", img)
